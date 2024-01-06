b7efcc627072e647e9e021e6852feac802a3e9f1
ahmedhalac <ahmedhalac@outlook.com>
2024-01-06 22:54:08 +0100

    Created login endpoint

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,47 +1,74 @@
+require("dotenv").config();
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const UserRoles = require("../enums/roles");
+const jwt = require("jsonwebtoken");
 
-const UserController = {
-  async register(req, res) {
-    try {
-      const {
-        first_name,
-        last_name,
-        username,
-        email,
-        password,
-        city,
-        phone_number,
-      } = req.body;
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      city,
+      phone_number,
+    } = req.body;
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      let role = UserRoles.USER;
+    let role = UserRoles.USER;
 
-      if (req.body.email === "adminvexor@gmail.com") {
-        role = UserRoles.ADMIN;
-      }
-
-      const newUser = await User.create({
-        first_name,
-        last_name,
-        username,
-        email,
-        password: hashedPassword,
-        city,
-        phone_number,
-        role,
-      });
-
-      res
-        .status(201)
-        .json({ message: "User created successfully", user: newUser });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
+    if (req.body.email === "adminvexor@gmail.com") {
+      role = UserRoles.ADMIN;
     }
-  },
+
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      username,
+      email,
+      password: hashedPassword,
+      city,
+      phone_number,
+      role,
+    });
+
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = UserController;
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+      expiresIn: 86400,
+    });
+
+    res.status(200).json({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,8 +3,5 @@ const middleware = require("../middleware");
 
 module.exports = function (app) {
   app.post("/register", middleware.checkExistingEmail, controller.register);
-
-  app.get("/admin", middleware.roleCheckMiddleware(["admin"]), (req, res) => {
-    res.send("This is the admin page");
-  });
+  app.post("/login", controller.login);
 };
