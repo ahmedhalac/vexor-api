f22c5299bcb9bea2ea104b116098f87d8828744c
ahmedhalac <ahmedhalac@outlook.com>
2024-01-07 23:17:20 +0100

    Removed app.js and moved its content to the server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
-const app = require("./src/app.js");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Vexor app" });
+});
+
+require("./src/routes/authRoutes")(app);
 
 const port = process.env.PORT || 3000;
-
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
